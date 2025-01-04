6f76a74cbcdf831b20b86d1684781c8747a5d60e
mayartmal <mayartmal@gmail.com>
2025-01-04 19:08:25 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+user_prompt = "Enter todo: "
+todos = list()
+
+
+while True:
+    todo = input(user_prompt)
+    print(todo.title())
+    todos.append(todo)
+    print(todos)
+
+
+while 2>1 :
+    pass
