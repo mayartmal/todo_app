8fcc2da04e1c87b969ef111d8fc0c8e050c32991
mayartmal <mayartmal@gmail.com>
2025-01-06 00:40:40 +0400

    enumerate in for loop

--- main.py
+++ main.py
@@ -3,16 +3,37 @@ todos = list()
 
 def logic():
     while True:
-        user_action = input("Type 'add' or 'show' or 'exit': ")
+        user_action = input("Type 'add' or 'show/display' or 'exit': ")
+        user_action = user_action.strip()
 
         match user_action:
             case "add":
                 todo = input("Enter a todo: ")
                 todos.append(todo)
-            case "show":
-                print(todos)
+            case "show" | "display":
+                for i, t in enumerate(todos):
+                    print(f"{i+1}) ", t.title())
+            case "edit":
+                if len(todos) == 0:
+                    print("Nothing to edit")
+                else:
+                    todo_number = int(input("Input number of todo to edit: ")) - 1
+                    print("Existing todo is:", todos[todo_number])
+                    new_todo = input("Enter a new todo: ")
+
+                    todos[todo_number] = new_todo
+            case 'complete':
+                if len(todos) == 0:
+                    print("Nothing to complete")
+                else:
+                    todo_number = int(input("Input number of todo to complete: ")) - 1
+                    todos.pop(todo_number)
+
             case "exit":
                 break
+            case _:
+                print("Unknown command...")
+
 
 
 
---
+++ tests/system_pytest/test_app.py
@@ -0,0 +1,5 @@
+import pytest
+import main
+
+def test_user_action_string():
+    pass
