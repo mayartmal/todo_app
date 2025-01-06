03b4ab5c84c0835d9a7b57cb6afdc7b502132673
mayartmal <mayartmal@gmail.com>
2025-01-06 19:49:50 +0400

    add txt file storage. fix add and read case

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 
-todos = list()
 
 def logic():
     while True:
-        user_action = input("Type 'add' or 'show/display' or 'exit': ")
+        user_action = input("Type 'add', 'edit', 'complete' or 'show/display' or 'exit': ")
         user_action = user_action.strip()
 
         match user_action:
             case "add":
-                todo = input("Enter a todo: ")
+                todo = input("Enter a todo: ") + "\n"
+                file = open('todos.txt', 'r')
+                todos = file.readlines()
+                file.close()
                 todos.append(todo)
+                file = open('todos.txt', 'w')
+                file.writelines(todos)
+                file.close()
             case "show" | "display":
+                file = open('todos.txt', 'r')
+                todos = file.readlines()
+                file.close()
                 for i, t in enumerate(todos):
                     print(f"{i+1}) ", t.title())
             case "edit":
@@ -20,7 +28,6 @@ def logic():
                     todo_number = int(input("Input number of todo to edit: ")) - 1
                     print("Existing todo is:", todos[todo_number])
                     new_todo = input("Enter a new todo: ")
-
                     todos[todo_number] = new_todo
             case 'complete':
                 if len(todos) == 0:
@@ -28,7 +35,6 @@ def logic():
                 else:
                     todo_number = int(input("Input number of todo to complete: ")) - 1
                     todos.pop(todo_number)
-
             case "exit":
                 break
             case _:
