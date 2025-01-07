d1c24a7a7efca0dde7713c1209f3f13861ca3ccb
mayartmal <mayartmal@gmail.com>
2025-01-07 22:17:08 +0400

    add list comprehensions and fix show case

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
-
 def logic():
     while True:
+        # get user action and remove space from user action string
         user_action = input("Type 'add', 'edit', 'complete' or 'show/display' or 'exit': ")
         user_action = user_action.strip()
         match user_action:
@@ -18,9 +17,20 @@ def logic():
                 file = open('files/todos.txt', 'r')
                 todos = file.readlines()
                 file.close()
+                # below is the way to remove \n from hole list with comprehensions
+                # new_todos = [item.strip('\n') for item in todos]
+                # !!! here is list comprehensions logic
+                # format  new var = [item.action() for item in items]
+                # new_todos = []
+                # for t in todos:
+                #     new_todos.append(t.strip('\n'))
                 for i, t in enumerate(todos):
-                    print(f"{i+1}) ", t.title())
+                    t = t.strip('\n')
+                    row = f"{i+1}) {t.title()}"
+                    print(row)
             case "edit":
+                file = open('files/todos.txt', 'r')
+                todos = file.readlines()
                 if len(todos) == 0:
                     print("Nothing to edit")
                 else:
@@ -29,6 +39,8 @@ def logic():
                     new_todo = input("Enter a new todo: ")
                     todos[todo_number] = new_todo
             case 'complete':
+                file = open('files/todos.txt', 'r')
+                todos = file.readlines()
                 if len(todos) == 0:
                     print("Nothing to complete")
                 else:
